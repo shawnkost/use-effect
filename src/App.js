14ebb9b4b27962b0d7911dc67d6abcfb1f101829
shawnkost <shawnmkost@gmail.com>
2021-03-02 17:41:41 -0800

    learned use-effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
 
-function App() {
+export default function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  // const [items, setItems] = useState([])
+
+  useEffect(() => {
+    console.log("resource changed")
+    return () => {
+      console.log("return from resource changed");
+    }
+  }, [resourceType])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
+  )
 }
-
-export default App;
